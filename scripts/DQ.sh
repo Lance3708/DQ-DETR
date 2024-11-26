728937cd189ea6f3ec0cd7c08682a5cb7485c062
lawchli <lawchli3708@gmail.com>
2024-11-26 19:43:40 +0900

    first training

--- scripts/DQ.sh
+++ scripts/DQ.sh
@@ -1,6 +1,7 @@
 coco_path=/home/lance/workspace/AITOD-Dataset/aitod_mini
 python /home/lance/workspace/DQ-DETR/main_aitod.py \
-  --output_dir logs/DQDETR_mini_5epo_1 -c /home/lance/workspace/DQ-DETR/config/DQ_5scale.py --coco_path $coco_path \
+  --output_dir logs/DQDETR_mini_5epo_1 -c /home/lance/workspace/DQ-DETR/config/DQ_5scale.py \
+  --coco_path $coco_path \
   --pretrain_model_path /home/lance/workspace/DQ-DETR/pretrained/pretrain_model.pth \
   --options dn_scalar=100 embed_init_tgt=False \
   dn_label_coef=1.0 dn_bbox_coef=1.0 use_ema=False \
--- scripts/DQ_eval.sh
+++ scripts/DQ_eval.sh
@@ -1,11 +1,13 @@
-checkpoint=$1
-python DQ-DETR/main_aitod.py \
-  --output_dir DQ-DETR/logs/DQ_eval \
-	-c DQ-DETR/config/DQ_5scale.py --coco_path AITOD-Dataset/aitod \
-	--eval --resume $checkpoint \
+checkpoint=$50
+python /home/lance/workspace/DQ-DETR/main_aitod.py \
+    --output_dir /home/lance/workspace/DQ-DETR/scripts/logs/DQDETR_mini_5epo_1 \
+	-c /home/lance/workspace/DQ-DETR/config/DQ_5scale.py \
+	--coco_path /home/lance/workspace/AITOD-Dataset/aitod_micro/ \
+	--eval \
+	--resume $checkpoint \
 	--options dn_scalar=100 embed_init_tgt=False \
 	dn_label_coef=1.0 dn_bbox_coef=1.0 use_ema=False \
-	dn_box_noise_scale=1.0
+	dn_box_noise_scale=1.0\
 
 
 # checkpoint=$1
